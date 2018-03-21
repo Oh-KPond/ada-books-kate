0964057784e1417bd4a7a3e779690ba356e82e97
Kate Pond <kate@thepondsedge.com>
2018-03-21 09:36:21 -0700

    updates routes order so that create new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show', as: 'book'
 
   # the following two should always go together
   get '/books/new', to: 'books#new', as: 'new_book'
@@ -9,7 +8,9 @@ Rails.application.routes.draw do
   # the following two should always go together
   get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
   patch '/books/:id', to: 'books#update'
-  
+
+  get '/books/:id', to: 'books#show', as: 'book'
+
   delete '/books/:id', to: 'books#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
