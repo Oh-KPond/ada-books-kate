e75e943dd5de18c443327c1b009f4d7f8caf8240
Kate Pond <kate@thepondsedge.com>
2018-04-02 16:23:43 -0700

    creates a db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author_list = [
+  {
+    name: "A Person"
+  },
+  {
+    name: "An Animal"
+  },
+  {
+    name: "Someone"
+  }
+]
+
+author_list.each do |author|
+  worked = Author.create(author)
+  puts "#{author[:name]} was ID: #{worked.id}!"
+end
+
+
+
+book_list = [
+  {
+    title: "This is a Book",
+    description: "description needed",
+    author_id: 1
+  },
+  {
+    title: "This is Another Book",
+    description: "description needed",
+    author_id: 2
+  },
+  {
+    title: "Here, Have Another Book",
+    description: "description needed",
+    author_id: 3
+  }
+]
+
+book_list.each do |book|
+  worked = Book.create(book)
+  puts "#{book[:title]} was ID: #{worked.id}!"
+end
