b296d423072b6ee114b1efd4ff54bcfc8aab1666
Kate Pond <kate@thepondsedge.com>
2018-04-02 10:54:42 -0700

    removes author column from Book DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320174306) do
+ActiveRecord::Schema.define(version: 20180402174903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
