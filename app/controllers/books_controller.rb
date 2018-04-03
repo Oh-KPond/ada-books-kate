b86445a01be11c3ca5b3ee77aba61634127b1b3e
Kate Pond <kate@thepondsedge.com>
2018-04-03 10:44:33 -0700

    adds error handling for validation when creating a book without a title

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # book.title = params[:book][:title]
     # book.author = params[:book][:author]
     # book.description = params[:book][:description]
 
-    if book.save # it worked!
+    if @book.save # it worked!
       redirect_to books_path
     else
       render :new
