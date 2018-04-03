26e9909cfb3d1348b5d59d5cbd6ff67b7d484a27
Kate Pond <kate@thepondsedge.com>
2018-04-03 10:02:32 -0700

    adds resources to routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,21 @@ Rails.application.routes.draw do
   root 'books#index'
   # root_path exists
 
-  get '/books', to: 'books#index'
+  resources :books
 
-  # the following two should always go together
-  get '/books/new', to: 'books#new', as: 'new_book'
-  post '/books', to: 'books#create'
-
-  # the following two should always go together
-  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
-  patch '/books/:id', to: 'books#update'
-
-  get '/books/:id', to: 'books#show', as: 'book'
-
-  delete '/books/:id', to: 'books#destroy'
+  # get '/books', to: 'books#index'
+  #
+  # # the following two should always go together
+  # get '/books/new', to: 'books#new', as: 'new_book'
+  # post '/books', to: 'books#create'
+  #
+  # # the following two should always go together
+  # get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  # patch '/books/:id', to: 'books#update'
+  #
+  # get '/books/:id', to: 'books#show', as: 'book'
+  #
+  # delete '/books/:id', to: 'books#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
