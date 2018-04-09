79f05cdf4e32e6932b68e16076f6d2198f4b8045
Kate Pond <kate@thepondsedge.com>
2018-04-09 13:02:08 -0700

    fixes method in Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,13 +1,19 @@
 class Author < ApplicationRecord
   has_many :books
 
-# Dee built this method that does NOT work
   def first_published
-    my_books =
-    self.books.order(:publication_year)
+    books_with_publication_date =
+    self.books.where.not(publication_year: nil)
 
-    first_book = my_books.first
+    sorted_books =
+    books_with_publication_date.order(publication_year: asc)
 
-    return first_book.publication_year
+    if sorted_books.empty?
+      return "NO BOOKS"
+    end
+
+    first_book = sorted_books.first
+
+    return first_book.publication_year.to_i
   end
 end
