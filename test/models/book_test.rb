51ffb2cede5a4d0dbabed20baf5b7a3223257be0
Kate Pond <kate@thepondsedge.com>
2018-04-09 13:30:30 -0700

    creates fixture data

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -44,6 +44,20 @@ class BookTest < ActiveSupport::TestCase
 
       book.genres.must_include genre
     end
+    
+  # below uses fixtures
+    describe 'relations' do
+      it "has an author" do
+        book = books(:poodr)
+        book.author.must_equal authors(:metz)
+      end
+
+      it "can set the author" do
+        book = Book.new(title: "test book")
+        book.author = authors(:metz)
+        book.author_id.must_equal authors(:metz).id
+      end
+    end
 
   end
 end
