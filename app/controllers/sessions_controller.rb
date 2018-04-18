cf45db41cf114c2011876e13125490389ce288d4
Kate Pond <kate@thepondsedge.com>
2018-04-18 09:28:18 -0700

    creates login with OAuth Github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,40 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by name: params[:user][:name]
+    auth_hash = request.env['omniauth.auth']
 
-    if @user
+    if auth_hash[:uid]
+      @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+      if @user.nil?
+        # User doesn't match anything in the DB
+        # Attempt to create a new user
+        @user = User.build_from_github(auth_hash)
+      else
+        flash[:success] = "Logged in successfully"
+        redirect_to root_path
+      end
+
+      # If we get here, we have the user instance
       session[:user_id] = @user.id
-      # session[:user_name] = @user.name
-      flash[:success] = "Welcome back #{@user.name}"
     else
-      @user = User.create name: params[:user][:name]
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome #{@user.name}"
+      flash[:error] = "Could not log in"
+      redirect_to root_path
     end
-    redirect_to root_path
+    # @user = User.find_by name: params[:user][:name]
+    #
+    # if @user
+    #   session[:user_id] = @user.id
+    #   # session[:user_name] = @user.name
+    #   flash[:success] = "Welcome back #{@user.name}"
+    # else
+    #   @user = User.create name: params[:user][:name]
+    #   session[:user_id] = @user.id
+    #   flash[:success] = "Welcome #{@user.name}"
+    # end
+    # redirect_to root_path
+  end
+
+  def index
+    @user = User.find(session[:user_id]) # < recalls the value set in a previous request
   end
 
   def new
@@ -20,7 +43,11 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You logged out!"
-    redirect_to root_path
+        flash[:success] = "Successfully logged out!"
+
+        redirect_to root_path
+    # session[:user_id] = nil
+    # flash[:success] = "You logged out!"
+    # redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  def self.build_from_github(auth_data)
+    return User.create(
+      provider: auth_data[:provider],
+      uid: auth_data[:uid],
+      email: auth_data[:info][:email],
+      name: auth_data[:info][:nickname]
+    )
+  end
 end
